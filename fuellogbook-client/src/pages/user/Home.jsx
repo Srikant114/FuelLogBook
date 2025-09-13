b72486356d15e1106734c06c3bf8b14d4c81bf70
Srikant114 <srikantsahu114@gmail.com>
2025-09-13 14:45:52 +0530

    feat-OFe : Implement user navigation and theme context
    - Added NavbarUser component for user navigation with responsive design.
    - Created ThemeContext for managing light and dark themes.
    - Integrated theme toggle functionality in the Navbar.
    - Defined navigation links in navLinks.js for easy management.
    - Set up global styles with Tailwind CSS and custom font.
    - Created basic structure for admin and user pages including Dashboard, FuelLogs, Report, Users, Vehicles, Login, and Register.
    - Updated main entry point to include ThemeContextProvider.
    - Added logo assets for branding.

---
+++ fuellogbook-client/src/pages/user/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavbarUser from '../../components/user/NavbarUser'
+
+const Home = () => {
+  return (
+    <>
+        <NavbarUser/>
+    </>
+  )
+}
+
+export default Home
